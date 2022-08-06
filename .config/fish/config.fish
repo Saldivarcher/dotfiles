edcb3cb793f95b500d7740f8dd64d39015de352e
Miguel Saldivar <miguel.saldivar@hpe.com>
2022-08-06 10:55:19 -0700

    Minor updats to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     source $HOME/.config/fish/alias.fish
-    set -x PATH "/cray/css/compiler/cost/testing/bin" $PATH
+    set -x PATH "$HOME/.local/bin" $PATH
+    set -x PATH "$HOME/bin" $PATH
+
+    switch (uname)
+        case Linux
+            set -x PATH "/cray/css/compiler/cost/testing/bin" $PATH
+        case Darwin
+            set -x PATH "/Users/migsaldivar/Library/Python/3.9/bin" $PATH
+    end
 end
